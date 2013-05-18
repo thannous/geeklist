14786aa643421291e8137e0b29b46d480b9e805c
Loïc Saint-Roch <contact@loicsaintroch.fr>
2013-05-18 13:37:13 +0200

    lib

--- Gekklist/mapMicros/MapViewController.m
+++ Gekklist/mapMicros/MapViewController.m
@@ -45,8 +45,8 @@
 - (void)loadView {
     // Create a GMSCameraPosition that tells the map to display the
     // coordinate -33.86,151.20 at zoom level 6.
-    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.86
-                                                            longitude:151.20
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:47.86
+                                                            longitude:2.20
                                                                  zoom:6];
     mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
     mapView_.myLocationEnabled = YES;
@@ -54,7 +54,7 @@
     
     // Creates a marker in the center of the map.
     GMSMarker *marker = [[GMSMarker alloc] init];
-    marker.position = CLLocationCoordinate2DMake(-33.86, 151.20);
+    marker.position = CLLocationCoordinate2DMake(47.8, 2.20);
     marker.title = @"Sydney";
     marker.snippet = @"Australia";
     marker.map = mapView_;
